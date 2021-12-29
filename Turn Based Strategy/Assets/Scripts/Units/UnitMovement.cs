0060a7c1722ea861f7fc53d313b14e5a32b598dc
Kog <47494397+Kyle-Gian@users.noreply.github.com>
2021-12-29 18:51:10 +1100

    Unit movement changed from navmesh

--- Turn Based Strategy/Assets/Scripts/Units/UnitMovement.cs
+++ Turn Based Strategy/Assets/Scripts/Units/UnitMovement.cs
@@ -4,37 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
 public class UnitMovement : MonoBehaviour
 {
-    private NavMeshAgent agent;
+    public float unitSpeed = 2f;
     int index;
     public event Action OnPathComplete;
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
-    
+    private Vector3 posToMoveTowards;
     public void MoveUnit(List<Vector3> path)
     {
-        if (agent != null)
-        {
-            agent.SetDestination(path[index]);
-        }
-        if (index != path.Count - 1)
-        {
-            if (agent.remainingDistance <= 0.02f)
-            {
-                index++;
-                agent.SetDestination(path[index]);
-            }
-        }
-        else
+        float step = unitSpeed * Time.deltaTime;
+        
+        if (posToMoveTowards == Vector3.zero)
+            posToMoveTowards = new Vector3(path[index].x, path[index].y + .5f, path[index].z);
+        
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, posToMoveTowards, step);
+        
+
+        if (Vector3.Distance(transform.position, posToMoveTowards) <= 0.001f)
+            index++;
+        
+        if(index == path.Count)
         {
             index = 0;
+            posToMoveTowards = Vector3.zero;
             path.Clear();
             OnPathComplete?.Invoke();
         }
+        else
+            posToMoveTowards = new Vector3(path[index].x, path[index].y + 1f, path[index].z);
     }
 }
